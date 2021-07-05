3b291c85e8e82876fd22fa3941e081880d934c45
PatrickSpace <jplosta01@gmail.com>
2021-07-04 19:51:07 -0500

    routes and contreollers init

--- src/routes/analisisRoutes.js
+++ src/routes/analisisRoutes.js
@@ -2,9 +2,19 @@ const express = require("express");
 const router = express.Router();
 const analisisController = require("../controllers/analisisController");
 
+//************ IMPORTANTE **************/
+// ruta: /analisis
+
 //test
 router.get("/", analisisController.test);
 
+//texto de pdf
+router.get("/pdf", analisisController.text);
+
+//acceso a microservicio
 router.post("/preguntar", analisisController.preguntar);
 
+router.get("/preguntar", (req, res) => {
+  res.send("preguntar funciona");
+});
 module.exports = router;
