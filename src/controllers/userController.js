28e072f56aabeb13e257994b735022e6bc828e43
Diegolivia <diegolivia@hotmail.com>
2021-08-28 21:41:17 -0500

    UserController has been refactored to allow better getters

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,154 +2,166 @@ const User = require("../models/User");
 const Rol = require("../models/Rol");
 const jwt = require("jsonwebtoken");
 
-const errors = async (nombre, usuario, password, roles) => {
-  const errorsarray = [];
-  if (!nombre || nombre === "") errorsarray.push("Ingrese un nombre");
-  if (!usuario || usuario === "") errorsarray.push("Ingrese un usuario");
-  if (!password || password === "") errorsarray.push("Ingrese una contraseña");
-  if (!roles || roles.length === 0) errorsarray.push("Ingrese almenos un rol");
-  if (usuario) {
-    const userFound = await User.findOne({ usuario: usuario });
-    if (userFound) {
-      errorsarray.push("Este nombre de usuario ya está en uso");
+const errors = async(nombre, usuario, password, roles) => {
+    const errorsarray = [];
+    if (!nombre || nombre === "") errorsarray.push("Ingrese un nombre");
+    if (!usuario || usuario === "") errorsarray.push("Ingrese un usuario");
+    if (!password || password === "") errorsarray.push("Ingrese una contraseña");
+    if (!roles || roles.length === 0) errorsarray.push("Ingrese almenos un rol");
+    if (usuario) {
+        const userFound = await User.findOne({ usuario: usuario });
+        if (userFound) {
+            errorsarray.push("Este nombre de usuario ya está en uso");
+        }
     }
-  }
-  if (roles) {
-    const foundRoles = await Rol.find({ nombre: { $in: roles } });
-    if (foundRoles.length === 0) {
-      errorsarray.push("Los roles no son validos");
+    if (roles) {
+        const foundRoles = await Rol.find({ nombre: { $in: roles } });
+        if (foundRoles.length === 0) {
+            errorsarray.push("Los roles no son validos");
+        }
     }
-  }
-  return errorsarray;
+    return errorsarray;
 };
 
-module.exports = {
-  getAll: async function (req, res) {
-    try {
-      const lista = await User.find(
-        {},
-        "proyectos _id nombre usuario"
-      ).populate("roles");
-      res.status(200).json({ items: lista });
-    } catch (err) {
-      console.log(err);
+const lookForRole = async(rol) => {
+    switch (rol) {
+        case "profesor":
+            val = '60eb439908800d0318b9aa70';
+            break;
+        case "director":
+            val = '60eb439908800d0318b9aa71';
+            break;
+        case "coordinador":
+            val = '60eb439908800d0318b9aa72';
+            break;
     }
-  },
-  getProfesores: async function (req, res) {
-    try {
-      const usuarios = await User.find(
-        {},
-        "proyectos _id nombre usuario"
-      ).populate("roles");
-      let profes = [];
-      if (usuarios.length > 0) {
-        usuarios.forEach((user) => {
-          user.roles.forEach((rol) => {
-            if (rol.nombre === "profesor") {
-              profes.push(user);
-            }
-          });
-        });
-      }
-      res.status(200).json({ items: profes });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  getCoordinadores: async function (req, res) {
-    const usuarios = await User.find(
-      {},
-      "proyectos _id nombre usuario"
-    ).populate("roles");
-    let profes = [];
-    if (usuarios.length > 0) {
-      usuarios.forEach((user) => {
-        user.roles.forEach((rol) => {
-          if (rol.nombre === "coordinador") {
-            profes.push(user);
-          }
-        });
-      });
-    }
-    res.status(200).json({ items: profes });
-  },
-  getDirectores: async function (req, res) {
-    const usuarios = await User.find(
-      {},
-      "proyectos _id nombre usuario"
-    ).populate("roles");
-    let profes = [];
-    if (usuarios.length > 0) {
-      usuarios.forEach((user) => {
-        user.roles.forEach((rol) => {
-          if (rol.nombre === "director") {
-            profes.push(user);
-          }
-        });
-      });
-    }
-    res.status(200).json({ items: profes });
-  },
-  userbyID: async function (req, res) {
-    try {
-      const userfound = await User.findById(
-        req.params.id,
-        "proyectos _id nombre usuario"
-      );
-      if (!userfound) {
-        res.status(400).json({ msg: ["Este proyecto no existe"] });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(400).json({ msg: ["Ocurrio un error"] });
-    }
-  },
-  createuser: async function (req, res) {
-    try {
-      const { nombre, usuario, password, roles } = req.body;
-      const errores = await errors(nombre, usuario, password, roles);
-      if (errores.length === 0) {
-        const newUser = new User({
-          nombre,
-          usuario,
-          password: await User.encryptPassword(password),
-        });
-        const foundRoles = await Rol.find({ nombre: { $in: roles } });
-        newUser.roles = foundRoles.map((rol) => rol._id);
-        const savedUser = await newUser.save();
+    const usuarios = await User.find({ "roles": { $elemMatch: { "$eq": (val) } } }).populate("roles");
+    return usuarios;
+}
 
-        res.status(200).json({ msg: "Usuario creado exitosamente" });
-      } else {
-        res.status(400).json({ msg: errores });
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(400).json({ msg: ["usuario no creado"] });
-    }
-  },
-  delete: async function (req, res) {
-    try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json({ msg: "Usuario borrado exitosamente" });
-    } catch (err) {
-      console.log(err);
-      res.status(400).json({ msg: ["Ocurrió un error"] });
-    }
-  },
-  update: async function (req, res) {
-    try {
-      const { nombre, usuario, roles } = req.body;
-      const foundRoles = await Rol.find({ nombre: { $in: roles } });
-      let finalroles = foundRoles.map((rol) => rol._id);
-      await User.findByIdAndUpdate(req.params.id, {
-        nombre,
-        usuario,
-        finalroles,
-      });
-      res.status(200).json({ msg: "Usuario actualizado exitosamente" });
-    } catch (err) {
-      console.log(err);
-      res.status(400).json({ msg: ["Ocurrió un error"] });
-    }
-  },
+module.exports = {
+    getAll: async function(req, res) {
+        try {
+            const lista = await User.find({},
+                "proyectos _id nombre usuario"
+            ).populate("roles");
+            res.status(200).json({ items: lista });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    getProfesores: async function(req, res) {
+        try {
+            usuarios = await lookForRole("profesor");
+            let profes = [];
+            if (usuarios.length > 0) {
+                usuarios.forEach((user) => {
+                    if (!(user.roles.some(function(rol) { return rol.nombre === "director" || rol.nombre === "coordinador"; }))) {
+                        profes.push(user)
+                    };
+                });
+            } else {
+                res.status(406).json({ msg: "No existen usuarios con este rol" });
+            }
+            res.status(200).json({ items: profes });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    getCoordinadores: async function(req, res) {
+        try {
+            usuarios = await lookForRole("coordinador");
+            let profes = [];
+            if (usuarios.length > 0) {
+                usuarios.forEach((user) => {
+                    if (!(user.roles.some(function(rol) { return rol.nombre === "director" }))) {
+                        profes.push(user)
+                    };
+                });
+            } else {
+                res.status(406).json({ msg: "No existen usuarios con este rol" });
+            }
+            res.status(200).json({ items: profes });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    getDirectores: async function(req, res) {
+        try {
+            usuarios = await lookForRole("director");
+            let profes = [];
+            if (usuarios.length > 0) {
+                usuarios.forEach((user) => {
+                    profes.push(user)
+                });
+            } else {
+                res.status(406).json({ msg: "No existen usuarios con este rol" });
+            }
+            res.status(200).json({ items: profes });
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    userbyID: async function(req, res) {
+        try {
+            const userfound = await User.findById(req.params.id);
+            if (!userfound) {
+                res.status(400).json({ msg: ["Este usuario no existe"] });
+            }
+            userfound["password"] = ""
+            console.log(userfound);
+            res.status(200).json({ items: userfound });
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({ msg: ["Ocurrio un error"] });
+        }
+    },
+    createuser: async function(req, res) {
+        try {
+            const { nombre, usuario, password, roles } = req.body;
+            const errores = await errors(nombre, usuario, password, roles);
+            if (errores.length === 0) {
+                const newUser = new User({
+                    nombre,
+                    usuario,
+                    password: await User.encryptPassword(password),
+                });
+                const foundRoles = await Rol.find({ nombre: { $in: roles } });
+                newUser.roles = foundRoles.map((rol) => rol._id);
+                const savedUser = await newUser.save();
+
+                res.status(200).json({ msg: "Usuario creado exitosamente" });
+            } else {
+                res.status(400).json({ msg: errores });
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({ msg: ["usuario no creado"] });
+        }
+    },
+    delete: async function(req, res) {
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json({ msg: "Usuario borrado exitosamente" });
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({ msg: ["Ocurrió un error"] });
+        }
+    },
+    update: async function(req, res) {
+        try {
+            const { nombre, usuario, roles } = req.body;
+            const foundRoles = await Rol.find({ nombre: { $in: roles } });
+            let finalroles = foundRoles.map((rol) => rol._id);
+            await User.findByIdAndUpdate(req.params.id, {
+                nombre,
+                usuario,
+                finalroles,
+            });
+            res.status(200).json({ msg: "Usuario actualizado exitosamente" });
+        } catch (err) {
+            console.log(err);
+            res.status(400).json({ msg: ["Ocurrió un error"] });
+        }
+    },
 };
