816e3695b4c4fb89cea892469e558719eb4f76d2
Diegolivia <diegolivia@hotmail.com>
2021-09-09 00:41:11 -0500

    Added get not asigned projects

--- src/controllers/proyectoController.js
+++ src/controllers/proyectoController.js
@@ -2,19 +2,20 @@ const Proyecto = require("../models/Proyecto");
 const Documento = require("../models/Documento");
 const User = require("../models/User");
 const pdfjs = require("pdfjs-dist/legacy/build/pdf.js");
+const { compareSync } = require("bcryptjs");
 
-function parsetxt(stream){
-  return stream.promise.then(function(pdf){
+function parsetxt(stream) {
+  return stream.promise.then(function (pdf) {
     var mpages = pdf._pdfInfo.numPages;
     var arrPromises = [];
-    for (var i = 1; i <= mpages ; i++){
-      arrPromises.push(pdf.getPage(i).then(function(page){
-        return page.getTextContent().then(function(text){
-          return text.items.map(function(s){return s.str;}).join('');
+    for (var i = 1; i <= mpages; i++) {
+      arrPromises.push(pdf.getPage(i).then(function (page) {
+        return page.getTextContent().then(function (text) {
+          return text.items.map(function (s) { return s.str; }).join('');
         });
       }));
     }
-    return Promise.all(arrPromises).then(function(txt){
+    return Promise.all(arrPromises).then(function (txt) {
       return txt.join('');
     })
   });
@@ -122,23 +123,23 @@ module.exports = {
       res.status(400).json({ msg: "ocurrió un error" });
     }
   },
-  UploadDoc: async function(req, res){
+  UploadDoc: async function (req, res) {
     try {
       let data = new Uint8Array(req.file.buffer)
       let stream = await pdfjs.getDocument(data);
       var doc = await parsetxt(stream);
-      console.log("Doc uploaded => size: "+ Buffer.byteLength(doc));
+      console.log("Doc uploaded => size: " + Buffer.byteLength(doc));
       let name = req.file.originalname;
-      const newdoc = new Documento({nombre:name,texto:doc});
+      const newdoc = new Documento({ nombre: name, texto: doc });
       const proyectofound = await Proyecto.findById(req.params.id);
       const docsaved = await newdoc.save();
       proyectofound.documentos.push(docsaved._id);
       const proyectosaved = await proyectofound.save();
-      return res.status(200).json({ msg: "documento guardado",documento: docsaved, proyecto: proyectosaved._id });
+      return res.status(200).json({ msg: "documento guardado", documento: docsaved, proyecto: proyectosaved._id });
 
     } catch (error) {
       console.log(error);
-      return res.status(400).json({msg:"ocurrió un error"});
+      return res.status(400).json({ msg: "ocurrió un error" });
     }
   },
 
@@ -146,23 +147,24 @@ module.exports = {
     try {
       const users = await User.find();
       const projs = await Proyecto.find();
-      console.log(projs[0].id);
-      var test = [];
+      var UsrIDs = [];
+      var free = [];
       users.forEach(user => {
-       test=test.concat(user.proyectos)
-      });
-      var free=[]
-      projs.forEach(proj => {
-        if(!(test.includes(proj.id))){
-          free.push(proj);
+        if(user!=''){
+          user.proyectos.forEach(pr=>{
+            UsrIDs = UsrIDs.concat(pr.toString())
+          })
         }
       });
-      return res.status(200).json({items:free});
+      projs.forEach(proj => {
+        if(UsrIDs.indexOf(proj.id)==-1){
+          free.push(proj)
+        }
+      });
+      return res.status(200).json({ items: free });
     } catch (err) {
       console.log(err);
-      return res.status(400).json({msg:"Internal Error"});
+      return res.status(400).json({ msg: "Internal Error" });
     }
   }
-
-
 };
